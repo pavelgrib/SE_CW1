43976d137d4b288c42c7cc139b9ed37b6b362ae4
pg <paul.quant@gmail.com>
2012-11-21 16:34:14 +0000

    jmock junit, fit jars

---
+++ CW2/AcmeTelecom/src/tests/TestHtmlPrinter.java
@@ -0,0 +1,11 @@
+package tests;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pg
+ * Date: 11/21/12
+ * Time: 4:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestHtmlPrinter {
+}
