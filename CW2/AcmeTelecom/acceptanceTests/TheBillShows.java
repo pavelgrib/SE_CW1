0284df58dcb7c43f6a8553b0d767ad7eb173297e
Jun HE <junhe1026@gmail.com>
2012-11-23 12:24:46 +0000

    build fit file structure

---
+++ CW2/AcmeTelecom/acceptanceTests/TheBillShows.java
@@ -0,0 +1,20 @@
+import fit.RowFixture;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hejun
+ * Date: 12-11-23
+ * Time: 上午11:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class TheBillShows extends RowFixture{
+    @Override
+    public Object[] query() throws Exception {
+        return new Object[0];  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Class getTargetClass() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
