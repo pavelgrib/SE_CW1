de6ad5dccf6dc0489abd112c985e6054f7289b84
Jun HE <junhe1026@gmail.com>
2012-11-29 12:30:58 +0000

    finish rate logic without considering overnight calls

--- CW2/AcmeTelecom/src/com/acmetelecom/rate/PeakSeperateOffPeakRateEngine.java
+++ CW2/AcmeTelecom/src/com/acmetelecom/rate/PeakSeperateOffPeakRateEngine.java
@@ -2,8 +2,11 @@ package com.acmetelecom.rate;
 
 import com.acmetelecom.Call;
 import com.acmetelecom.customer.Tariff;
+import org.omg.CORBA.BAD_INV_ORDER;
 
 import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,25 +14,95 @@ import java.math.BigDecimal;
  * Date: 12-11-28
  * Time: 下午12:02
  */
-public class PeakSeperateOffPeakRateEngine implements RateEngine {
+public class PeakSeperateOffPeakRateEngine /*implements RateEngine*/ {
 
-    private int startHour;
-    private int endHour;
+    // defaut peak time is 7:00 to 19:00
+    private int startHour = 7;
+    private int endHour = 19;
 
     public PeakSeperateOffPeakRateEngine(){
-        new PeakSeperateOffPeakRateEngine(7,19);
+
     }
 
     public PeakSeperateOffPeakRateEngine(int startHour, int endHour){
         this.startHour = startHour;
-        this.startHour = endHour;
+        this.endHour = endHour;
     }
 
-    @Override
-    public BigDecimal calculateCost(Call call,Tariff tariff) {
+    //@Override
+    public BigDecimal calculateCost(Call call/*，Tariff tariff*/) {
         BigDecimal cost = BigDecimal.ZERO;
+        DaytimePeakPeriod daytimePeakPeriod = new DaytimePeakPeriod(startHour, endHour);
+        int startHourInSec = startHour*3600;
+        int endHourInSec = endHour*3600;
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(call.startTime());
+        int sHour = calendar.get(Calendar.HOUR_OF_DAY);
+        int sMinute = calendar.get(Calendar.MINUTE);
+        int sSecond = calendar.get(Calendar.SECOND);
+        int startTimeInSec = sHour*3600+sMinute*60+sSecond;
+
+        calendar.setTime(call.endTime());
+        int eHour = calendar.get(Calendar.HOUR_OF_DAY);
+        int eMinute = calendar.get(Calendar.MINUTE);
+        int eSecond = calendar.get(Calendar.SECOND);
+        int endTimeInSec = eHour*3600+eMinute*60+eSecond;
 
 
-        return null;
+        System.out.println(startTimeInSec);
+        System.out.println("---"+startHourInSec);
+        System.out.println(endTimeInSec);
+        System.out.println("---"+endHourInSec);
+        // we ignore the situation that a call could last over 24 hours
+        // but we have to consider the call last overnight
+        if (eHour < sHour) {
+
+        } else {
+
+
+            // the call duration is all in the off peak time, we should notice that a call can last over the peak time
+            if (daytimePeakPeriod.offPeak(call.startTime()) && daytimePeakPeriod.offPeak(call.endTime()) &&
+                    call.durationSeconds() <= (endHourInSec - startHourInSec)) {
+                cost = cost.add(new BigDecimal(call.durationSeconds()).multiply(/*tariff.offPeakRate()*/ BigDecimal.ONE));
+            }
+            // the call duration is all in the peak time
+            else if (!daytimePeakPeriod.offPeak(call.startTime()) && !daytimePeakPeriod.offPeak(call.endTime())) {
+                cost = cost.add(new BigDecimal(call.durationSeconds()).multiply(/*tariff.peakRate()*/BigDecimal.TEN));
+            }
+            // the call crosses both off peak and peak time
+            else {
+                int offPeakTime = 0;
+                int peakTime = 0;
+                System.out.println("enter cross");
+                // cross the start of the peak
+                if (startTimeInSec <= startHourInSec && startHourInSec <= endTimeInSec && endTimeInSec <= endHourInSec){
+                    System.out.println("cross first");
+                    offPeakTime = startHourInSec-startTimeInSec ;
+                    peakTime = endTimeInSec-startHourInSec;
+                }
+                // cross the end of the peak
+                else if (startHourInSec <= startTimeInSec && startTimeInSec <= endHourInSec && endHourInSec <= endTimeInSec ){
+                    System.out.println("cross last");
+                    peakTime = endHourInSec - startTimeInSec;
+                    offPeakTime = endTimeInSec - endHourInSec ;
+                }
+                // cross whole peak
+                else if (sHour <= startHour && eHour >= endHour){
+                    System.out.println("all cross");
+                    peakTime = endHourInSec-startHourInSec;
+                    System.out.println("peaktime ="+ peakTime);
+                    offPeakTime = (startHourInSec-startTimeInSec) + (endTimeInSec-endHourInSec);
+                    System.out.println("offpeaktiem = "+offPeakTime);
+                }
+
+                cost = cost.add(new BigDecimal(offPeakTime).multiply(/*tariff.offPeakRate()*/BigDecimal.ONE));
+                cost = cost.add(new BigDecimal(peakTime).multiply(/*tariff.peakRate()*/BigDecimal.TEN));
+            }
+        }
+
+        System.out.println("Cost is ---------------- "+cost);
+        return cost;
     }
 }
--- CW2/AcmeTelecom/src/fake/FakeRunner.java
+++ CW2/AcmeTelecom/src/fake/FakeRunner.java
@@ -1,7 +1,13 @@
 package fake;
 
+import com.acmetelecom.Call;
+import com.acmetelecom.customer.Tariff;
+import com.acmetelecom.rate.PeakSeperateOffPeakRateEngine;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,7 +17,7 @@ import java.io.PrintStream;
  */
 public class FakeRunner {
 
-    public static void main(String arges[]){
+    public static void main(String arges[]) throws InterruptedException {
           ByteArrayOutputStream os = new ByteArrayOutputStream();
           //PrintStream ps = new PrintStream(os);
           FakePrinter fakePrinter = new FakePrinter(System.out);
@@ -19,21 +25,57 @@ public class FakeRunner {
 
           FakeBillingSystem bs = new FakeBillingSystem(fakeGenerator);
 
-        bs.setTime(System.currentTimeMillis());
-        bs.callInitiated("447722113434", "447766511332");
-        bs.setTime(bs.getTime()+20000);
-        bs.callCompleted("447722113434", "447766511332");
+//        bs.setTime(System.currentTimeMillis());
+//        bs.callInitiated("447722113434", "447766511332");
+//        bs.setTime(bs.getTime()+20000);
+//        bs.callCompleted("447722113434", "447766511332");
+//
+//        bs.setTime(System.currentTimeMillis());
+//        bs.callInitiated("447722113434", "447711111111");
+//        bs.setTime(bs.getTime()+30000);
+//        bs.callCompleted("447722113434", "447711111111");
+//
+//        bs.setTime(System.currentTimeMillis());
+//        bs.callInitiated("447777765432", "447711111111");
+//        bs.setTime(bs.getTime()+60000);
+//        bs.callCompleted("447777765432", "447711111111");
+//
+//        bs.createCustomerBills();
 
-        bs.setTime(System.currentTimeMillis());
-        bs.callInitiated("447722113434", "447711111111");
-        bs.setTime(bs.getTime()+30000);
-        bs.callCompleted("447722113434", "447711111111");
+//        Date a = new Date();
+//        Date b = new Date();
+//        System.out.println(">>>>>>>>>>>>>>>>> "+a.after(b));
+//
+        long time = 1354086000000l; // 2012-11-28 7:00
 
-        bs.setTime(System.currentTimeMillis());
-        bs.callInitiated("447777765432", "447711111111");
-        bs.setTime(bs.getTime()+60000);
-        bs.callCompleted("447777765432", "447711111111");
 
-        bs.createCustomerBills();
+        FakeCallStart fakeCallStart = new FakeCallStart("1","2",(time-3600000));
+        FakeCallEnd fakeCallEnd = new FakeCallEnd("1","2", (time+3600000+12*3600000));
+        Call call = new Call(fakeCallStart,fakeCallEnd);
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(call.startTime());
+        Date startDate = calendar.getTime();
+        int sHour = calendar.get(Calendar.HOUR_OF_DAY);
+        int sMinute = calendar.get(Calendar.MINUTE);
+        int sSecond = calendar.get(Calendar.SECOND);
+
+        calendar.setTime(call.endTime());
+        Date endDate = calendar.getTime();
+        int eHour = calendar.get(Calendar.HOUR_OF_DAY);
+        int eMinute = calendar.get(Calendar.MINUTE);
+        int eSecond = calendar.get(Calendar.SECOND);
+
+        System.out.println(">>>>>>>>"+sHour+"  "+sMinute+"  "+sSecond);
+        System.out.println(">>>>>>>>"+eHour+"  "+eMinute+"  "+eSecond);
+        PeakSeperateOffPeakRateEngine engine = new PeakSeperateOffPeakRateEngine();
+        engine.calculateCost(call);
+
+
+//
+//        System.out.println(System.currentTimeMillis());
+//        Thread.sleep(10 * 1000);
+//        System.out.println(System.currentTimeMillis());
+
     }
 }
